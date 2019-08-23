e20e83b6cd6a8b318ce61a8b1f4f054eb3bcea09
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-08-23 11:17:58 +0100

    work in progress

--- src/parts/font-style/index.js
+++ src/parts/font-style/index.js
@@ -6,13 +6,7 @@ import structure from '../../structure'
 let template = fs.readFileSync(__dirname + '/template.sqrl', 'utf8')
 
 export default ['template', 'font-style', ({ theme }) => {
-	// let data = {
-	// 	rules: [{
-	// 		name: 'font-style',
-	// 		abbr: 'font',
-	// 		...structure(theme.font.style, ['modifiers', 'props', 'value'])
-	// 	}]
-	// }
+
 	let o = theme.font.style
 	let string = ''
 	for (let modifier in o) {
@@ -22,5 +16,6 @@ export default ['template', 'font-style', ({ theme }) => {
 		}
 		string += `}\n`
 	}
+
 	return string
 }]
