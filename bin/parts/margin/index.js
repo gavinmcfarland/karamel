11faa00945837ac2295fa4a158933ee9839c16f6
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-08-27 11:03:22 +0100

    added scale output to margin

--- bin/parts/margin/index.js
+++ bin/parts/margin/index.js
@@ -8,7 +8,18 @@ var _default = ['template', 'margin', function (_ref) {
   var theme = _ref.theme;
   var name = 'margin';
   var abbr = theme.property.margin.abbr;
-  return ".".concat(abbr, " {\n\t").concat(name, ": var(--").concat(name, ", unset);\n}\n.").concat(abbr, " > * {\n\t--").concat(name, ": unset;\n}");
+  var o = theme.scale.spacing;
+  var string = '';
+
+  for (var i = 0; i < o.length; i++) {
+    var modifier = o[i];
+    string += ".".concat(abbr, "-").concat(i, " {\n");
+    string += "\tmargin: ".concat(modifier, "\n");
+    string += "}\n";
+  }
+
+  string += ".".concat(abbr, " {\n\t").concat(name, ": var(--").concat(name, ", unset);\n}\n.").concat(abbr, " > * {\n\t--").concat(name, ": unset;\n}");
+  return string;
 }];
 exports["default"] = _default;
 module.exports = exports.default;
