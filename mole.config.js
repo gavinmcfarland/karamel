ac845db900082e4fd510351ada3118dd102dfe73
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-08-27 14:11:47 +0100

    removed font

--- mole.config.js
+++ mole.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	theme: 'theme.jsonnet',
-	template: ['width', 'margin', 'max-width', 'padding', 'font', 'font-size', 'flex', 'font-style'],
+	template: ['width', 'margin', 'max-width', 'padding', 'font-size', 'flex', 'font-style'],
 	output: { file: '/dist/karamel.css' }
 }
--- src/parts/font/index.js
+++
@@ -1,7 +0,0 @@
-export default ['template', 'max-width', theme => {
-
-	return `\
-	.font-${modifier} {
-		font-family: ;
-	}`
-}]
